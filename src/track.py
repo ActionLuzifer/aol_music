4caeb6fcb821b470233299c38c70a62aa64c1739
ActionLuzifer <ActionLuzi@yahoo.de>
2011-07-20 14:18:17 +0200

    wget wird durch python-interne Funktionen ersetzt

--- src/track.py
+++ src/track.py
@@ -1,4 +1,5 @@
 import os
+import urllib.request
 
 class Track:
     '''
@@ -20,11 +21,17 @@ class Track:
         self.m_url       = self.m_trackJSONobj['url']; #"http:\/\/serve.castfire.com\/audio\/653505\/friday-is-forever_2011-06-29-181705.cdlp.flv",
         self.m_duration  = self.m_trackJSONobj['duration']; #"184"
 
+
+    def priv_urlToStringData(self, url):
+        return urllib.request.urlopen(url).read();
+
+
     def pub_downloadTo(self, mf_path, mf_really):
-        fileStr = self.m_album.pub_replaceBadChars("{}_-_{}_-_{:0>2}_-_{}.flv".format(self.m_album.getartist_name(), self.m_album.getalbum_name(), self.m_trackNr, self.m_title))
-        executeStr = "wget {} -O{}/".format(self.m_url, mf_path) + fileStr
+        fileStr = "{}_-_{}_-_{:0>2}_-_{}.flv".format(self.m_album.getartist_name(), self.m_album.getalbum_name(), self.m_trackNr, self.m_title)
         if mf_really :
-            os.system(executeStr)
+            myTrackFile = open("{0}/{1}".format(mf_path, fileStr), 'wb')
+            myTrackFile.write(self.priv_urlToStringData(self.m_url))
+            myTrackFile.close()
         else:
-            print(executeStr)
+            print("{0} -> {1}/{2}".format(self.m_url, mf_path, fileStr))
         
