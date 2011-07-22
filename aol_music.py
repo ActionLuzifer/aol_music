c15a6a70861b16d0abd96acd7079b2af92f6b1ec
ActionLuzifer <ActionLuzi@yahoo.de>
2011-07-22 18:20:08 +0200

    jetzt richtiger Ordner, vorher war der basepath abgeschnitten

--- aol_music.py
+++ aol_music.py
@@ -20,7 +20,7 @@ def priv_urlToString(url):
     return str(htmldings.read().decode('utf-8'));
 
 def priv_checkForPath(mf_subPath):
-    mypath = os.path.normpath(os.path.dirname("{0}".format(os.getcwd())) + mf_subPath)
+    mypath = os.path.normpath("{0}".format(os.getcwd()) + mf_subPath)
     if not os.path.exists(mypath):
         os.system("mkdir {0}".format(mypath))
     return mypath
