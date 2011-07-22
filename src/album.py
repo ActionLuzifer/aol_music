d0ba25b082d00d6bc81a8553c15052a781074f9d
ActionLuzifer <ActionLuzi@yahoo.de>
2011-07-22 19:05:40 +0200

    bei einem Download des Albums wird jetzt auch das Cover mit in den DownloadOrdner geladen

--- src/album.py
+++ src/album.py
@@ -1,6 +1,5 @@
 import os
 import track
-import sys
 import public_functions
 
 class Album:
@@ -96,12 +95,18 @@ class Album:
         return self._count;
     
     def pub_downloadTo(self, mf_path, mf_really):
-        fileStr = "{}_-_{}_-_{:0>2}.json".format(self.getartist_name().replace(" ", "_"), self.getalbum_name().replace(" ", "_"), "00")
-        myfileName = public_functions.pub_getOSFilenameStr(os.path.normpath("{0}/{1}".format(mf_path, fileStr)))
+        # download json-file
+        fileStr = "{}_-_{}_-_{:0>2}".format(self.getartist_name().replace(" ", "_"), self.getalbum_name().replace(" ", "_"), "00")
+        myfileName = public_functions.pub_getOSFilenameStr(os.path.normpath("{0}/{1}.json".format(mf_path, fileStr)))
         myfile = open(myfileName, 'w')
         myfile.write(self.m_albumjsonFormattedStr)
         myfile.close()
-
+        # kopiere Cover
+        myCoverName = public_functions.pub_getOSFilenameStr(os.path.normpath("{0}/{1}.jpg".format(mf_path, fileStr)))
+        myCover = open(myCoverName, 'wb')
+        myCover.write(public_functions.pub_urlToStringData(self.getalbum_thumbnail()))
+        myCover.close()
+        
         for mytrack in self._queueOfTrack:
             trackfileStr = public_functions.pub_getOSFilenameStr("{}_-_{}_-_{:0>2}_-_{}.flv".format(self.getartist_name(), self.getalbum_name(), mytrack.m_trackNr, mytrack.m_title))
             mytrack.pub_downloadTo(mf_path, mf_really, trackfileStr);
